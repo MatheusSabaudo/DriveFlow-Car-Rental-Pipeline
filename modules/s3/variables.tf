7d6251562ce350e0842d6060d9d1e1b49bb29ed5
Matheus Sabaudo <matteosabaudo@outlook.it>
2026-07-13 20:25:17 +0200

    feat: added S3 buckets: raw | cleansed | curated | scripts

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -3,3 +3,21 @@ variable "raw_bucket_name" {
     type        = string
     default     = "driveflow-raw"
 }
+
+variable "cleansed_bucket_name" {
+    description = "Name of the cleansed S3 bucket"
+    type        = string
+    default     = "driveflow-cleansed"
+}
+
+variable "curated_bucket_name" {
+    description = "Name of the curated S3 bucket"
+    type        = string
+    default     = "driveflow-curated"
+}
+
+variable "scripts_bucket_name" {
+    description = "Name of the scripts S3 bucket"
+    type        = string
+    default     = "driveflow-scripts"
+}
